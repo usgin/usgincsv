f54316542027e1c957b41295c644d7bd8f43e0ac
Ryan Clark <ryan.clark.j@gmail.com>
2011-05-05 19:19:49 -0700

    Added CsvUpload model, some crappy tests for the file saving.

--- models.py
+++ models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.core.files.storage import FileSystemStorage
+import datetime, os, csv
 
-# Create your models here.
+ROOT_DATA_LOCATION = os.path.abspath(os.path.join(os.path.dirname(__file__), 'data'))
+DATA_STORE = FileSystemStorage(location=ROOT_DATA_LOCATION, base_url='/csv-uploads/')
+
+def data_location(instance, filename):
+    gen_user = instance.return_email.split('@')[0]
+    return os.path.join(gen_user, filename)
+
+class CsvUpload(models.Model):
+    csv_file = models.FileField(upload_to=data_location,
+                                storage=DATA_STORE,
+                                help_text='Allowed extensions: .csv')
+    return_email = models.EmailField()
+    upload_time = models.DateTimeField(blank=True, null=True)
+    
+    def __unicode__(self):
+        return str(self.upload_time) + ' -- Reply to: ' + str(self.return_email)
+
+    def clean(self):
+        # Set the Upload Time
+        self.upload_time = datetime.datetime.now()
+        
+        # Make sure that the uploaded file is valid CSV
+        try:
+            csv.DictReader(self.csv_file)
+        except:
+            raise ValidationError('Uploaded file is not a valid CSV file.')
--- tests.py
+++ tests.py
@@ -1,16 +1,52 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+import os, shutil, models as MODELS
+from django.core.exceptions import ValidationError
+from django.core.files.base import ContentFile
 
+class UploadClassTest(TestCase):
+    def setUp(self):
+        self.test_file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'test-files'))
+        self.valid_test_file = os.path.join(self.test_file_path, 'valid-test.csv')
+        self.invalid_test_file = os.path.join(self.test_file_path, 'invalid-test.txt')
+        self.valid_email = 'testing@fake.com'
+        self.gen_user = self.valid_email.split('@')[0]
+    
+    # Some of these tests are kinda lame. I should probably be testing POSTs to the admin interface instead
+    # of testing the model methods themselves.
+        
+    def test_valid_cleaning(self):
+        new_upload = MODELS.CsvUpload(csv_file=self.valid_test_file,
+                                      return_email=self.valid_email)
+        # Save method should call clean?
+        try:
+            new_upload.full_clean()
+        except ValidationError, ex:
+            self.fail('Valid inputs did not pass clean method. Exception was raised: %s' % str(ex))
+        
+        self.assertNotEqual(new_upload.upload_time, None, 'Upload time was not assigned properly.')
+            
+    def test_invalid_cleaning(self):
+        new_upload = MODELS.CsvUpload(csv_file=self.invalid_test_file,
+                                      return_email=self.valid_email)
+        self.assertRaises(ValidationError, new_upload.full_clean())
+        
+    def test_valid_file_placement(self):
+        new_upload = MODELS.CsvUpload(csv_file=self.valid_test_file,
+                                      return_email=self.valid_email)
+        new_upload.full_clean()
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+        file_content = ContentFile(open(new_upload.csv_file.url).read())
+        new_upload.csv_file.save('valid-test.csv', file_content)
+
+        # File ought to be in the right place now
+        try:
+            open(os.path.join(MODELS.ROOT_DATA_LOCATION, self.gen_user, 'valid-test.csv'))
+        except:
+            self.fail('File was not placed in the appropriate location after being saved.')
+        
+    def tearDown(self):
+        test_data_path = os.path.join(MODELS.ROOT_DATA_LOCATION, self.gen_user)
+        if os.path.exists(test_data_path):
+            shutil.rmtree(test_data_path)
+        
+        
